73ad73a89e227b64c70974890cdac52691a7eb75
konrad121970 <konrad121970@windowslive.com>
2021-11-02 22:41:53 +0100

    Dodano Zadanie 2

---
+++ Zadanie_2/Zadanie2/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  Zadanie2
+//
+//  Created by student on 12/10/2021.
+//  Copyright © 2021 pb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)enter {
+    NSString *yourName = self.inputField.text;
+    NSString *myName = @"Konrad";
+    NSString *message;
+    
+    if([yourName length] == 0) {
+        yourName = @"World";
+    }
+    
+    if([yourName isEqualToString:myName]) {
+        message = [NSString stringWithFormat:@"Hello %@! We have the same name :)", yourName];
+    }else {
+        message = [NSString stringWithFormat:@"Hello %@!", yourName];
+    }
+    
+    self.messageLabel.text = message;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue*)segue sender:(id)sender {
+    if([segue.identifier isEqualToString:@"sendSurnameSegue"]) {
+        SecondViewController *controller = (SecondViewController *) segue.destinationViewController;
+        
+        controller.delegate = self;
+        controller.surname = self.inputFieldTwo.text;
+    }
+}
+
+- (void)addItemViewController:(SecondViewController *)controller didFinishEnteringItem:(NSString *)item {
+    self.inputFieldTwo.text = item;
+}
+
+@end
