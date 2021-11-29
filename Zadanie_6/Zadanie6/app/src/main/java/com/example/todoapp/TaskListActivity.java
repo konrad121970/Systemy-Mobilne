c21950ceb38f74c6dd09460729b06a2cf0814270
konrad121970 <konrad121970@windowslive.com>
2021-11-29 19:05:02 +0100

    Dodano Zadanie 6

---
+++ Zadanie_6/Zadanie6/app/src/main/java/com/example/todoapp/TaskListActivity.java
@@ -0,0 +1,22 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
