c7796f2a86579ac2bf772763fe8985f01978488a
konrad121970 <konrad121970@windowslive.com>
2021-12-20 19:27:33 +0100

    Added Zadanie 10

---
+++ Zadanie_10/app/src/main/java/com/konrad/zadanie_10/BookService.java
@@ -0,0 +1,10 @@
+package com.konrad.zadanie_10;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface BookService {
+    @GET("search.json")
+    Call<BookContainer> findBooks(@Query("q") String query);
+}
